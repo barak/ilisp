14869568ea1f8f4fd2320619a46ddbd6b4de1cf6
marcoxa <marcoxa>
2002-05-23 20:30:25 +0000

    Made provisions to load `ilfsf21'. Added a doc string.  Does XEmacs have a variable like `emacs-minor-version'?

--- ilcompat.el
+++ ilcompat.el
@@ -8,36 +8,41 @@
 ;;; Please refer to the file ACKNOWLEGDEMENTS for an (incomplete) list
 ;;; of present and past contributors.
 ;;;
-;;; $Id: ilcompat.el,v 1.3 2001/05/12 22:10:53 marcoxa Exp $
+;;; $Id: ilcompat.el,v 1.4 2002/05/23 20:30:25 marcoxa Exp $
 
 (require 'cl)
 
 ;;; Global definitions/declarations
 
 (defconst +ilisp-emacs-version-id+
-    (cond ((string-match "XEmacs" emacs-version)
-	   'xemacs)
-	  ((string-match "Lucid" emacs-version)
-	   (if (string-match "^19.[0-7][^0-9]" emacs-version)
-	       'lucid-19
-	       'lucid-19-new))
-	  ((string-match "^19" emacs-version)
-	   'fsf-19)
-	  ((string-match "^2[01]" emacs-version)
-	   'fsf-20)
-	  (t 'fsf-18))
-  "The version of Emacs ILISP is running in.
-Declared as '(member fsf-19 fsf-19 fsf-20 lucid-19 lucid-19-new xemacs).
+  (cond ((string-match "XEmacs" emacs-version)
+	 'xemacs)
+	((string-match "Lucid" emacs-version)
+	 (if (string-match "^19.[0-7][^0-9]" emacs-version)
+	     'lucid-19
+	   'lucid-19-new))
+	((string-match "^19" emacs-version)
+	 'fsf-19)
+	((string-match "^20" emacs-version)
+	 'fsf-20)
+	((string-match "^21" emacs-version)
+	 'fsf-21)
+	(t 'fsf-18))
+  "The major version of (X)Emacs ILISP is running in.
+Declared as '(member fsf-19 fsf-19 fsf-20 fsf-21 lucid-19 lucid-19-new xemacs).
 Set in ilcompat.el.")
 
+
 (defconst +ilisp-emacs-minor-version-number+
-    (cond ((eq +ilisp-emacs-version-id+ 'fsf-18) 59)
-	  ((or  (eq +ilisp-emacs-version-id+ 'lucid-19)
-		(eq +ilisp-emacs-version-id+ 'lucid-19-new)
-		)
-	   12)				; Does emacs-minor-version work?
-	  ((eq +ilisp-emacs-version-id+ 'xemacs) 14)
-	  (t emacs-minor-version)))
+  (cond ((eq +ilisp-emacs-version-id+ 'fsf-18) 59)
+	((or  (eq +ilisp-emacs-version-id+ 'lucid-19)
+	      (eq +ilisp-emacs-version-id+ 'lucid-19-new)
+	      )
+	 12)			      ; Does emacs-minor-version work?
+	((eq +ilisp-emacs-version-id+ 'xemacs) 14)
+	(t emacs-minor-version))
+  "The minor version of (X)Emacs ILISP is running in.
+Set in ilcompat.el.")
 
 
 ;;; Load Emacs version specific compatibility modules
@@ -49,6 +54,7 @@ Set in ilcompat.el.")
       ((eq +ilisp-emacs-version-id+ 'fsf-18) (load "ilfsf18"))
       ((eq +ilisp-emacs-version-id+ 'fsf-19) (load "ilfsf19"))
       ((eq +ilisp-emacs-version-id+ 'fsf-20) (load "ilfsf20"))
+      ((eq +ilisp-emacs-version-id+ 'fsf-21) (load "ilfsf21"))
       )
 
 
