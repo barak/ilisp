2982e18653e0ec937f1b027614137876f17fe5a3
kevinrosenberg <kevinrosenberg>
2003-01-30 15:08:28 +0000

    Automatic commit for debian_version_5_12_0+cvs_2003_01_30

--- cmulisp.lisp
+++ cmulisp.lisp
@@ -10,7 +10,7 @@
 ;;; Please refer to the file ACKNOWLEGDEMENTS for an (incomplete) list
 ;;; of present and past contributors.
 ;;;
-;;; $Id: cmulisp.lisp,v 1.10 2002/09/12 01:54:31 rgrjr Exp $
+;;; $Id: cmulisp.lisp,v 1.11 2003/01/30 15:08:28 kevinrosenberg Exp $
 
 
 (in-package :ilisp)
@@ -25,13 +25,13 @@
 ;;; This fixes that problem, which only occurs in the CMU cores of this year.
 ;;;
 
-(defvar *Fix-pipe-streams* T
+(defvar *Fix-pipe-streams* t
   "Set to Nil if you want them left alone.  And tell me you don't get stuck.")
 
 (when (and *Fix-pipe-streams*
 	   (lisp::synonym-stream-p *terminal-io*)
 	   (eq (lisp::synonym-stream-symbol *terminal-io*)
-	       'SYSTEM::*TTY*))
+	       'system::*tty*))
   (setf *terminal-io* (make-two-way-stream system::*stdin* system::*stdout*))
   ;; *query-io* and *debug-io* are synonym streams to this, so this fixes
   ;; everything.
